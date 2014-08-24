76e5b8f570627cb8299c2ecee646fbd51d638754
neur0ghost <miguelatcp@gmail.com>
2014-08-24 20:12:27 +0100

    The most final version

--- cachematrix.R
+++ cachematrix.R
@@ -3,18 +3,24 @@ makeCacheMatrix <- function(x = matrix()){
 	set <- function(y){
 		x <<- y
 		m <<- NULL
+	# set the value of the matrix
 	}
 	get <- function()x
 	setmatrix <- function(solve) m <<- solve
+	#Assigning the function that inverts squared matrices
 	getmatrix <- function() m
 	list (set = set, get = get, setmatrix = setmatrix, getmatrix = getmatrix)
+	# Return the functions as a list
 }
 
 cacheSolve <- function(x,...){
+	# First, checking to see if the inverse has been calculated
 	m <- x§getmatrix()
 	if(!is.null(m)){
+		#If it has already been calculated
 		message("Master, I have that written in a paper somewhere")
-		return (m)
+		#A witty message so your day gets a little bit less monotonous.
+		return (m) # Return the inverse that was already calculated
 	}
 	data <- x$get()
 	m <- solve(data, ...)
